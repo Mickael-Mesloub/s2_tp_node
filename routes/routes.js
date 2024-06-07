57e9e44421aea8a4cf89bef9c6e98e207f876fc7
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-07 14:53:51 +0200

    feat: add pug files and render functions

--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,14 @@ import express from 'express';
 import { login, register } from '../controllers/authController.js';
 import { renderDashboard } from '../controllers/dashboardController.js';
 import { verifyToken } from '../middlewares/authMiddlewares.js';
-import { renderHome } from '../controllers/home.js';
+import { renderHome } from '../controllers/page_renderers/home.js';
+import { renderLogin } from '../controllers/page_renderers/login.js';
 const router = express.Router();
 
 router.get('/dashboard', verifyToken, renderDashboard);
-router.post('/', register);
 router.post('/login', login);
+router.get('/login', renderLogin);
+router.post('/', register);
 router.get('/', renderHome);
 
 export default router;
