5ef335557970b472bf3bef5eb4aaaf2640658f3b
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-08 11:55:33 +0200

    fix(verifyToken): user was not logged out when token expired

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -5,13 +5,19 @@ const { JWT_SECRET } = process.env;
 
 export const verifyToken = async (req, res, next) => {
   const { token } = req.session;
+  const { auth } = req.session;
 
+  // if no token is found, it means that user is not logged in
   if (!token) {
     return res.status(401).json({ message: 'Veuillez vous authentifier' });
   }
 
+  // if token is found, check if it is still valid
   await jwt.verify(token, JWT_SECRET, async (error, decoded) => {
     if (error) {
+      console.error(error);
+      // if it is not valid anymore, logout user
+      auth.isLogged = false;
       return res.status(403).json({ error: 'Veuillez vous reconnecter' });
     }
     req.userId = decoded.id;
