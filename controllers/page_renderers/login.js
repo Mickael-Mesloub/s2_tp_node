7c05df78a89b9fcec0c6e60675dbe2bc2f894d33
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-08 15:35:09 +0200

    feat(flash messages): add warning flash messages when token not found and token expired

--- controllers/page_renderers/login.js
+++ controllers/page_renderers/login.js
@@ -2,10 +2,11 @@ export const renderLogin = (req, res) => {
   const { auth } = req.session;
   const flashSuccess = req.flash('success');
   const flashErrors = req.flash('errors');
+  const flashWarnings = req.flash('warnings');
 
   if (auth && auth.isLogged) {
     return res.status(301).redirect('/dashboard');
   }
 
-  res.render('login', { flashSuccess, flashErrors });
+  res.render('login', { flashSuccess, flashErrors, flashWarnings });
 };
--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -9,16 +9,19 @@ export const verifyToken = async (req, res, next) => {
 
   // if no token is found, it means that user is not logged in
   if (!token) {
-    return res.status(401).json({ message: 'Veuillez vous authentifier' });
+    const askLoginMsg = 'Veuillez vous authentifier';
+    req.flash('warnings', askLoginMsg);
+    return res.status(301).redirect('/login');
   }
 
   // if token is found, check if it is still valid
   await jwt.verify(token, JWT_SECRET, async (error, decoded) => {
     if (error) {
-      console.error(error);
       // if it is not valid anymore, logout user
       auth.isLogged = false;
-      return res.status(403).json({ error: 'Veuillez vous reconnecter' });
+      const loginAgainMsg = 'Veuillez vous reconnecter';
+      req.flash('warnings', loginAgainMsg);
+      return res.status(301).redirect('/login');
     }
     req.userId = decoded.id;
     next();
