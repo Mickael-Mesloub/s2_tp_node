b7c82f0c1826416d734e6eca3a40b2774ac261f2
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-06-09 19:32:07 +0200

    docs: add comment for password regex

--- validation/validationRules.js
+++ validation/validationRules.js
@@ -1,4 +1,4 @@
 export const validationRules = {
   nameFields: /^[a-zA-ZÀ-ÿ][a-zA-ZÀ-ÿ'-]*[a-zA-ZÀ-ÿ]$/, // accepts only letters, hyphen and apostrophe
-  password: /^(?!.*[<>{}()\\`]).{6,255}$/,
+  password: /^(?!.*[<>{}()\\`]).{6,255}$/, // does not accept the following characters: : <, >, (, ), {, }, \\, et \``
 };
